4fbbee77b46c773e8219e5644d55b697505129c9
wyasuda <wataru.robo@gmail.com>
2014-05-29 23:19:38 +0900

    delete desktop-full

--- 00-create-cd.sh
+++ 00-create-cd.sh
@@ -87,7 +87,6 @@ apt-get -y install ros-$ROSDISTRO-turtlebot-viz
 apt-get -y install ros-$ROSDISTRO-turtlebot-simulator
 apt-get -y install ros-$ROSDISTRO-turtlebot-apps
 apt-get -y install ros-$ROSDISTRO-turtlebot
-apt-get -y install ros-$ROSDISTRO-desktop-full
 apt-get -y install ros-$ROSDISTRO-dynamixel-motor
 apt-get -y install git
 fi
