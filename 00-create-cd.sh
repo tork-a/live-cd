0f3163d8daea1a6aad34e76d54ac61f659b5270f
Kei Okada <kei.okada@gmail.com>
2015-01-26 22:19:39 +0900

    source ros setup for catkin

--- 00-create-cd.sh
+++ 00-create-cd.sh
@@ -93,6 +93,7 @@ rosdep install -r -n -y --rosdistro $ROSDISTRO --from-paths src --ignore-src
 # compile with catkin
 ## until catkin_tools 2.0.x (http://stackoverflow.com/questions/27969057/cant-launch-catkin-build-from-jenkins-job)
 (cd /tmp; apt-get install -qq -y python-setuptools; git clone https://github.com/catkin/catkin_tools; cd catkin_tools; python setup.py install)
+. /opt/ros/$ROSDISTRO/setup.sh
 catkin build
 cd -
 chown -R 999.999 /home/ubuntu/ros_ws
