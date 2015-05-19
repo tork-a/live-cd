2e5deb596b88dacd5023b43841fe73541eb06c95
Kei Okada <kei.okada@gmail.com>
2015-05-19 20:59:33 +0900

    cp /etc/skel to /home/ubuntu and source setup.bash

--- 00-create-cd.sh
+++ 00-create-cd.sh
@@ -78,6 +78,19 @@ apt-get -y -q install python-wstool python-rosdep python-catkin-tools
 apt-get -y -q install aptitude git ntp emacs
 rosdep init; rosdep update
 
+# make home directory
+mkdir -p /home/ubuntu/
+chown -R 999.999 /home/ubuntu/
+cp /etc/skel/.??* /home/ubuntu
+chown -R 999.999 /home/ubuntu/.??*
+echo "
+# ROS setup
+source /opt/ros/$ROS_DISTRO/setup.bash
+" >> /home/ubuntu/.bashrc
+HOME=/home/ubuntu rosdep update
+chown -R 999.999 /home/ubuntu/.ros
+
+# make catkin workspace
 mkdir -p /home/ubuntu/catkin_ws/src
 cd /home/ubuntu/catkin_ws
 wstool init src || echo "already initilized"
@@ -113,18 +126,18 @@ apt-get -y -q install ros-$ROSDISTRO-openni2-launch
 apt-get -y -q install ros-$ROSDISTRO-audio-common
 # For moveit
 apt-get -y -q install ros-$ROSDISTRO-moveit-ikfast
+apt-get -y -q install ros-$ROSDISTRO-moveit-full-pr2
 # qt-build
 apt-get -y -q install ros-$ROSDISTRO-qt-build
+# For Denso
+apt-get -y -q install ros-$ROSDISTRO-denso
 
 if [ ${ROSDISTRO} == "hydro" ]; then
 # RTM, Hiro-NXO
 apt-get -y -q install ros-$ROSDISTRO-hironx-tutorial
-# For Denso
-apt-get -y -q install ros-$ROSDISTRO-denso
 # For turtlebot
 apt-get -y -q install ros-$ROSDISTRO-turtlebot-viz
 # For moveit
-apt-get -y -q install ros-$ROSDISTRO-moveit-full-pr2
 apt-get -y -q install ros-$ROSDISTRO-industrial-desktop
 
 #rosemacs
